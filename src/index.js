c8551e931094253e1bce68a358588f1b9c35e0e2
TBrodiana <t.brodiana@gmail.com>
2022-08-29 09:19:47 +0300

    section home with slider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // Test import of a JavaScript module
 import { example } from '@/js/example'
 
+//Slider JS files (added by Tetiana)
+import 'js/_slick.min'
+import 'js/_slick.min'
 // Test import of an asset
 import webpackLogo from '@/images/webpack-logo.svg'
 
