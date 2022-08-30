1e5f8aee290415869fd7d42ccf2d15d1e0aba4a0
TBrodiana <t.brodiana@gmail.com>
2022-08-30 10:25:42 +0300

    fghfgh

--- src/index.js
+++ src/index.js
@@ -29,6 +29,31 @@ import '@/styles/index.scss'
 
 // const app = document.querySelector('#root')
 // app.append(logo, heading, imageBackground, imagePublic)
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+        dots: true,
+        slidesToShow: 1,
+        autoplay: true,
+        speed:3000,
+        autoplaySpeed:800,
+        responsive:[
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow:1
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow:1
+                }
+            }
+        ]
+    });
+});
+
 
 let divsMenuCard = document.querySelectorAll(".menu__card");
 
